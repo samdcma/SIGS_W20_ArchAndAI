3cf0a68f0cb761c95103433996e09ede36aa9fc6
samdcma <49687270+samdcma@users.noreply.github.com>
2020-10-22 15:06:13 -0400

    Delete run_container.sh

--- 2d_3d_style_dream_neural_renderer/run_container.sh
+++
@@ -1,5 +0,0 @@
-nvidia-docker run --rm -it \
-	-v /home/alexandracarlson/Desktop/Robotics_Building_networks/3Dmodels/:/3Dmodels:ro \
-	-v /home/alexandracarlson/Desktop/Robotics_Building_networks/2Dmodels:/2Dmodels:ro \
-	-v `pwd`:/root \
-	2d3d-dreamstyle
